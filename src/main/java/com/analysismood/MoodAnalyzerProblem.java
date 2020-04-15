5a1427d719e30ce3b10b992a0fd8b8a74036f97e
priyankagarad <garadpriyanka1995@gmail.com>
2020-04-15 13:50:52 +0530

    ADDED:Mood Analyzer return Sad and Happy Mood

--- src/main/java/com/analysismood/MoodAnalyzerProblem.java
+++ src/main/java/com/analysismood/MoodAnalyzerProblem.java
@@ -1,7 +1,29 @@
 package com.analysismood;
 public class MoodAnalyzerProblem
 {
-    public static void main(String args[])
+    String message;
+    public MoodAnalyzerProblem()
+    {
+        this.message=null;
+    }
+    //created constructor and pass string parameter
+    public MoodAnalyzerProblem(String message)
+    {
+        this.message=message;
+    }
+    //created method for checking mood
+    public String moodAnalyzerMethod(String mood)
+    {
+        if (message.contains("Happy"))
+        {
+            return "Happy";
+        } else if (message.contains("Sad"))
+        {
+            return "Sad";
+        }
+        return mood;
+    }
+    public static void main (String args[])
     {
         System.out.println("Welcome to Mood Analyzer");
     }
--- src/test/java/com/analysismood/MoodAnalyzerProblemTest.java
+++ src/test/java/com/analysismood/MoodAnalyzerProblemTest.java
@@ -1,3 +1,20 @@
 package com.analysismood;
-public class MoodAnalyzerProblemTest {
+import org.junit.Assert;
+import org.junit.Test;
+public class MoodAnalyzerProblemTest
+{
+    @Test
+    public void givenSadMoodMessage_shouldReturnSad()
+    {
+        MoodAnalyzerProblem mood = new MoodAnalyzerProblem("I am in Sad Mood");
+        String result = mood.moodAnalyzerMethod("");
+        Assert.assertEquals("Sad", result);
+    }
+    @Test
+    public void givenHappyMoodMessage_shouldReturnHappy()
+    {
+        MoodAnalyzerProblem mood = new MoodAnalyzerProblem("I am in Happy Mood");
+        String result = mood.moodAnalyzerMethod("");
+        Assert.assertEquals("Happy", result);
+    }
 }
